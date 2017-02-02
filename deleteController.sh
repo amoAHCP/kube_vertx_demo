177c462f4da067ee67084ba386bab642674b4b0e
Andy Moncsek <amo.ahcp@gmail.com>
2017-02-02 16:28:58 +0100

    update pom.xml

--- deleteController.sh
+++ deleteController.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 kubectl delete rc/frontend-verticle-dns-controller --namespace=$1
 kubectl delete rc/read-verticle-dns-controller --namespace=$1
 kubectl delete rc/write-verticle-dns-controller --namespace=$1
